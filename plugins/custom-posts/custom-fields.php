98400347802c9884a657ed91d1146bf2b9d2879e
BENJAMIN MOODY <hireme@benjaminmoody.com>
2014-08-01 14:51:01 -0400

    * Replaced wordpress custom helper class with ACF Pro (note also still has option for ACF 4 if required) * Cleaned up how custom posts are created -- now use pure api code no longer use helper class

---
+++ plugins/custom-posts/custom-fields.php
@@ -0,0 +1,5 @@
+<?php
+/*
+Paste ACF Export Code Here
+*/
+?>
--- plugins/custom-posts/custom-posts.php
+++ plugins/custom-posts/custom-posts.php
@@ -2,37 +2,21 @@
 /**
 * custom-posts.php
 * 
-* Loads the Wordpress Cuztom Helper class
+* Register Custom post types and Taxonomies for use with current theme
 *
-* Also use this to include any custom post types you create for your project
-* 
-* https://github.com/Gizburdt/Wordpress-Cuztom-Helper/wiki
+* Use http://generatewp.com/ to generate code :)
 *
-* @url 		https://github.com/Gizburdt/Wordpress-Cuztom-Helper
 * @author	Ben Moody
 */
 
-/**
-* cuztom-helper
-* 
-* Includes the Wordpress Cuztom Helper inc file
-* This helper class allows quick and easy creation of custom Post Types and Meta Fields
-*
-* See the template file in theme-post-types folder for example on how to use this
-*
-* @author	Ben Moody
-*/
-function prso_init_cuztom_helper() {
+function prso_init_custom_post_tax_filter_class() {
 	
 	//Init vars
-	$file_path = plugin_dir_path( __FILE__ ) . "wordpress-cuztom-helper/cuztom.php";
+	$file_path = plugin_dir_path( __FILE__ ) . "class.custom-post-tax-filter.php";
 	
-	if( file_exists($file_path) ) {
-		require_once( $file_path );
-	}
-	
+	include_once( $file_path );
 }
-prso_init_cuztom_helper();
+prso_init_custom_post_tax_filter_class();
 
 /**
 * Init theme posts types
@@ -46,31 +30,15 @@ prso_init_cuztom_helper();
 function prso_init_theme_post_types() {
 	
 	//Init vars
+	global $prso_posttype_admin_icon;
 	$file_path = plugin_dir_path( __FILE__ ) . "theme-post-types";
+	
+	$prso_posttype_admin_icon = get_stylesheet_directory_uri() . '/images/admin/custom_post_icon.png';
 	
 	//include_once( $file_path . '/cuztom-post-type_TEMPLATE.php' );
 	
 }
 prso_init_theme_post_types();
-
-/**
-* Init theme meta boxes
-* 
-* Include the meta box files for you theme here to set them up
-*
-* See the template file in theme-meta-boxes folder for example on how to use this
-*
-* @author	Ben Moody
-*/
-function prso_init_theme_meta_boxes() {
-	
-	//Init vars
-	$file_path = plugin_dir_path( __FILE__ ) . "theme-meta-boxes";
-	
-	//include_once( $file_path . '/cuztom-meta-box_TEMPLATE.php' );
-	
-}
-prso_init_theme_meta_boxes();
 
 /**
 * Init theme taxonomies
@@ -91,4 +59,69 @@ function prso_init_theme_taxonomies() {
 }
 prso_init_theme_taxonomies();
 
+/**
+* Init theme custom fields
+* 
+* Include adv custom field export
+*
+* @author	Ben Moody
+*/
+function prso_init_acf_custom_fields() {
+	
+	//Init vars
+	$file_path = plugin_dir_path( __FILE__ ) . "custom-fields.php";
+	$acf_path = plugin_dir_path( __FILE__ ) . "/advanced-custom-fields/acf.php";
+	
+	define( 'ACF_LITE', true );
+	
+	include_once( $acf_path );
+	include_once( $file_path );
+	
+}
+//prso_init_acf_custom_fields();
+
+/**
+* Init theme custom fields -- PRO VERSION
+* 
+* Include adv custom field export
+*
+* @author	Ben Moody
+*/
+function prso_init_acf_PRO_custom_fields() {
+	
+	//Init vars
+	$file_path = plugin_dir_path( __FILE__ ) . "custom-fields.php";
+	$acf_path = plugin_dir_path( __FILE__ ) . "/advanced-custom-fields-pro/acf.php";
+	
+	include_once( $acf_path );
+	include_once( $file_path );
+	
+	//Setup acf paths
+	add_filter('acf/settings/path', 'prso_acf_settings_path');
+	add_filter('acf/settings/dir', 'prso_acf_settings_dir');
+	
+	//Disable backend user options
+	//add_filter('acf/settings/show_admin', '__return_false');
+	
+}
+prso_init_acf_PRO_custom_fields();
+
+function prso_acf_settings_path( $path ) {
+ 
+	// update path
+	$path = plugin_dir_path( __FILE__ ) . '/advanced-custom-fields-pro/';
+	
+	// return
+	return $path;
+	
+}
+function prso_acf_settings_dir( $dir ) {
+ 
+	// update path
+	$dir = plugin_dir_url( __FILE__ ) . '/advanced-custom-fields-pro/';
+	
+	// return
+	return $dir;
+	
+}
 ?>
